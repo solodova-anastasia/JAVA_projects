44a3283f4b57db2995c9af2e4b606e656e8a2f57
Anastasia_Sol <an.solodova29@gmail.com>
2023-07-01 13:58:14 +0700

    task_1

---
+++ 123.java
@@ -0,0 +1,11 @@
+public class kateti {
+    public static void main(String[] args) {
+     double a,b,c,s,p;
+     a = 3;
+     b = 4;
+     c = Math.sqrt(a*a+b*b);
+     s = (a*b)/2;
+     p = c+a+b;
+     System.out.println(s+"\n"+p);
+    }
+   }
